fb971b5c940eecc0566f3213336a03b01d5cc1f8
Mathias <64673135+snowthesnowman@users.noreply.github.com>
2023-11-22 17:10:37 +0100

    iug

--- prooblig3/figures.js/canvasCircle.js
+++ prooblig3/figures.js/canvasCircle.js
@@ -3,13 +3,17 @@ const ctx = figures.getContext('2d');
 figures.width = window.innerWidth;
 figures.height = window.innerHeight;
 
-function drawCircle(){
     ctx.fillStyle = 'black'; //colour og mode
     ctx.strokeStyle = 'white'; //mode
     ctx.lineWidth = 10;
-    ctx.beginPath();
-    ctx.arc(550, 600, 240, 0,  Math.PI * 2); //radius
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke(); 
-}
+   
+var radius = ctx.beginPath();
+        ctx.arc(550, 600, 240, 0,  Math.PI * 2); //radius
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke(); 
+
+
+
+
+function testcircle(radius, mode, colour)
