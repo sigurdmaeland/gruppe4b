98639f60a9a8a163edc802c06808ad4d21d53cc0
Mathias <64673135+snowthesnowman@users.noreply.github.com>
2023-11-22 15:40:54 +0100

    fdsf

---
+++ prooblig3/script/svgÖmer.js
@@ -0,0 +1,7 @@
+document.write('<svg width="600" height="400">');
+document.write('<rect x="200" y="200" width="200" height="150" fill="brown" />');
+document.write('<polygon points="200,200 300,100 400,200" fill="red" />');
+document.write('<rect x="275" y="280" width="50" height="70" fill="green" />');
+document.write('<rect x="220" y="230" width="50" height="40" fill="lightblue" />');
+document.write('<rect x="330" y="230" width="50" height="40" fill="lightblue" />');
+document.write('</svg>');
