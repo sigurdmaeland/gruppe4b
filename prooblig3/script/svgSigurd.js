0e52185dced1621195c770b5208a8eabda735cda
Mathias <64673135+snowthesnowman@users.noreply.github.com>
2023-11-22 16:09:28 +0100

    j

--- prooblig3/script/svgSigurd.js
+++ prooblig3/script/svgSigurd.js
@@ -1,11 +1,11 @@
 document.write('<svg width="500" height="500">');
-document.write('<circle cx="100" cy="155" r="60" stroke="white" stroke-width="3" fill="black" />');
-document.write('<circle cx="100" cy="110" r="55" stroke="white" stroke-width="3" fill="black" />');
-document.write('<circle cx="100" cy="55" r="50" stroke="white" stroke-width="3" fill="black" />');
+document.write('<circle cx="100" cy="185" r="80" stroke="white" stroke-width="3" fill="black" />');
+document.write('<circle cx="100" cy="120" r="65" stroke="white" stroke-width="3" fill="black" />');
 document.write('<circle cx="100" cy="55" r="50" stroke="white" stroke-width="3" fill="black" />');
 document.write('<circle cx="80" cy="45" r="7" stroke="white" stroke-width="3" fill="white" />');
 document.write('<circle cx="115" cy="45" r="7" stroke="white" stroke-width="3" fill="white" />');
 document.write('<rect x="83" y="80" width="30" height="1" style="fill:rgb(255, 255, 255);stroke-width:10;stroke:rgb(255, 255, 255)" />');
-document.write('<rect x="10" y="110" width="50" height="2" style="fill:rgb(0, 0, 0);stroke-width:10;stroke:rgb(0, 0, 0)" />  ');
-document.write('<rect x="140" y="110" width="50" height="2" style="fill:rgb(0, 0, 0);stroke-width:10;stroke:rgb(0, 0, 0)" />  ');
+document.write('<rect x="10" y="120" width="50" height="2" style="fill:rgb(0, 0, 0);stroke-width:10;stroke:rgb(0, 0, 0)" />  ');
+document.write('<rect x="140" y="120" width="50" height="2" style="fill:rgb(0, 0, 0);stroke-width:10;stroke:rgb(0, 0, 0)" />  ');
+document.write('<polygon points="90,65 105,65 98,55" fill="white" stroke="white"/>');
 document.write('</svg>');
